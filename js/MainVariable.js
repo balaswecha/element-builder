3c4c9ff11b77dc06fc549a487b683030778e6b87
Nikhil <nikhil@localhost.localdomain>
2015-03-17 15:18:03 +0530

    Added applications

---
+++ js/MainVariable.js
@@ -0,0 +1,117 @@
+var mainObject = {
+	"protons":0,
+	"neutrons":0,
+	"electron":0,
+	"name":"",
+	"ion":""
+	}
+	var electronImage= new Image();
+ 	electronImage.src="images/electron.png";
+
+function getConfiguration(electrons) {
+
+var Result = new Array();
+Result[0]=electrons;
+
+ 	for(i in json)
+ 	{ 
+
+     if(json[i].Protons===electrons){
+     	var configArray=json[i]["element configuration"];
+     	for(i=0;i<configArray.length;i++){
+     		if(Result[parseInt(configArray[i][0][0])]!=undefined)
+     		{
+     		Result[parseInt(configArray[i][0][0])]+=configArray[i][1];
+     	}
+     	else {
+     		Result[parseInt(configArray[i][0][0])]=0;
+     		Result[parseInt(configArray[i][0][0])]=configArray[i][1];
+     	}
+     	
+
+}
+
+ 	break;
+ 	}
+ 	
+ 	}
+ 	
+ 	return Result;
+ }
+
+
+
+function drawCanvas(){
+	
+	if (animationSet) {
+	
+	context.clearRect(0,0,canvas.width,canvas.height);
+	Result=getConfiguration(mainObject.electrons);
+
+ 	
+ 	
+ 	orbits=Result.length-1;
+ 	context.clearRect(0,0,canvas.width,canvas.height);
+ 	radius=80;
+ 	for (i=0;i<orbits;i++,radius+=30) {
+ 		context.beginPath();
+      context.arc(canvas.width/2,canvas.height/2,radius,0,2*Math.PI);
+      context.stroke();
+      for (j=0;j<Result[i+1];j++,angle+=angleBreakdown) {
+      if (j==0) {
+      angleBreakdown=360/Result[i+1];
+      angle=angleBreakdown+angleIncrease;      
+      }
+      pointX=(canvas.width/2)+ (radius*Math.cos(angle* Math.PI/180));
+      pointY=(canvas.height/2)+ (radius*Math.sin(angle* Math.PI/180));
+      context.drawImage(electronImage,pointX-15,pointY-15,30,30);
+      
+   }
+}
+angleIncrease+=0.8;
+
+
+      context.beginPath();
+      context.arc(canvas.width/2,canvas.height/2,50,0,2*Math.PI);
+      context.strokeStyle = '#000000';
+      context.stroke();
+      context.fillStyle = '#e1f318';
+      context.fill();
+     
+      context.beginPath();
+      context.arc((canvas.width/2)-0,(canvas.height/2)-17,26, 0, 2 * Math.PI); 
+      context.fillStyle='#d54532';
+      context.fill();
+      
+      
+      context.beginPath();
+      context.arc((canvas.width/2)-0,(canvas.height/2)+27,26, 0, 2 * Math.PI); 
+      context.fillStyle='#4f3ed4';
+      context.fill();
+     
+          
+      context.fillStyle = '#ffffff';	
+      context.font = "15pt Calibri";
+      context.fillText("P: "+mainObject.protons,(canvas.width/2)-20,(canvas.height/2)-10);
+      context.fillText("N: "+mainObject.neutrons,(canvas.width/2)-20,(canvas.height/2)+35);
+      
+      context.fillStyle = '#000000';
+		context.font = "20pt opensans";
+		context.fillText(mainObject.ion,490,45);
+		
+		context.fillStyle = '#000000';
+		context.font = "20pt Calibri";
+		context.fillText(mainObject.name,50,45);
+		      
+      
+      
+   }
+   
+   window.requestAnimationFrame(drawCanvas);
+      
+ }
+
+
+
+
+
---
+++ js/myscript.js
@@ -0,0 +1,126 @@
+
+var nucleas=new Image();
+var imageLoad=false;
+var animationSet=false;
+var angleIncrease=0.09;
+var pbutton=document.getElementById("pbutton");
+pbutton.addEventListener("click",protonconfiguration);
+var nbutton=document.getElementById("nbutton");
+nbutton.addEventListener("click",neutronconfiguration);
+var ebutton=document.getElementById("ebutton");
+ebutton.addEventListener("click",electronconfiguration);
+document.getElementById("submit").addEventListener("click", elementConfiguration, false );
+
+
+
+		canvas=document.getElementById("canvas");
+		context=canvas.getContext("2d");	
+
+
+
+function elementConfiguration()
+{
+ var electrons=parseInt(document.getElementById("electron").value);
+ var protons=parseInt(document.getElementById("proton").value);
+ var neutrons=parseInt(document.getElementById("neutron").value);
+ var result=document.getElementById("Result");
+ if(electrons===protons)
+ {
+ 	for(i in json)
+ 	{ 
+ 	if(json[i].Protons===electrons)
+ 	{
+ 	if(json[i].neutrons===neutrons)
+ 	{
+ 		var config = "";
+ 		for(j=0;j<json[i]["element configuration"].length;j++){
+ 			for(k=0;k<json[i]["element configuration"][j].length;k++){
+ 				if(k===1){
+ 				config +="<sup>"+json[i]["element configuration"][j][k]+"</sup>";
+ 			}
+ 			else{
+ 				config +=json[i]["element configuration"][j][k];
+ 				}
+ 				} 			
+ 			}
+ 		
+ 	result.innerHTML='<div id="elementname">'+json[i].name+" "+"["+json[i].symbol+"] -"+" "+config+'</div>';
+ 	break;
+ 	}
+ 	else{
+	result.innerHTML='<div id="alert">'+"This is not a Stable Element"+'</div>'; 	
+	break;
+ 		}
+ 	}
+ 	
+ 	}
+ 	
+ 	}
+ else
+ {
+	result.innerHTML='<div id="alert">'+"This is not a Stable Element"+'</div>';
+ 	}
+}
+
+function electronconfiguration(){
+	var electrons = parseInt(document.getElementById("electron").value);
+	mainObject.electrons=electrons;
+	setAtomType();
+	if (!animationSet) {
+		animationSet=true;
+		window.requestAnimationFrame(drawCanvas);
+	}
+	
+}
+
+function setAtomType() {
+		if (mainObject.electrons>mainObject.protons) {
+	mainObject.ion="-Ion";
+}
+	else if(mainObject.electrons<mainObject.protons){
+	mainObject.ion="+Ion";
+}
+	else if (mainObject.electrons==mainObject.protons) {
+	mainObject.ion="Neutral Atom";
+}
+}
+
+
+
+
+function protonconfiguration()
+
+{ 
+   var protons=parseInt(document.getElementById("proton").value);
+	mainObject.protons=protons;
+	setAtomType();
+	setName(protons);
+	if (!animationSet) {
+		animationSet=true;
+		window.requestAnimationFrame(drawCanvas);
+	}
+		
+
+
+}
+
+function setName(protons) {
+	for(i in json) {
+		if (json[i].Protons===parseInt(protons)) {
+			mainObject.name=json[i].name;
+			break;
+		}
+		}
+	}
+
+function neutronconfiguration()
+{
+	var neutrons=parseInt(document.getElementById("neutron").value);
+	mainObject.neutrons=neutrons;
+	if (!animationSet) {
+		animationSet=true;
+		window.requestAnimationFrame(drawCanvas);
+	}
+	
+	}
+  
